020603a184cc66df7394e2c92b179d5c1c5c3435
abdullah-dot-programmer <a22795101@gmail.com>
2024-10-10 18:15:07 +0500

    Files added at once

---
+++ script.js
@@ -0,0 +1,32 @@
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+const cutIcons = document.querySelectorAll("#cut");
+const pro = document.querySelector(".pro")
+
+if (bar) {
+    bar.addEventListener("click", () => {
+        nav.classList.add("active");
+    })
+}
+if (close) {
+    close.addEventListener("click", () => {
+        nav.classList.remove("active");
+    })
+}
+
+function cutAll() {
+    cut.parentElement.style.display = "none";
+}
+
+cutIcons.forEach(cut => {
+    cut.addEventListener("click", () => {
+        cut.parentElement.remove();
+    })
+})
+
+// const handleProClick = window.location.href = "sproduct.html"
+
+// pro.forEach(pro => {
+//     addEventListener("click", handleProClick)
+// })
