393d91bcd4cbaa21ed7059ef32b1e87e78985651
abdullah-dot-developer <abdullahdeveloper966@gmail.com>
2024-12-07 22:10:19 +0500

    Website Optimized

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const nav = document.getElementById("navbar");
 const cutIcons = document.querySelectorAll("#cut");
 const pro = document.querySelector(".pro")
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hero = document.getElementById("hero");
+    hero.style.backgroundImage = "url('img/hero4.webp')";
+    hero.style.backgroundSize = "cover";
+    hero.style.backgroundPosition = "top 25% right 0";
+})
+
 if (bar) {
     bar.addEventListener("click", () => {
         nav.classList.add("active");
